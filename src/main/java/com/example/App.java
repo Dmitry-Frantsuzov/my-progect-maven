7992f4721b38aac44e5a48c774caf8bc3e1b66df
Dmitry-Frantsuzov <Frantsyz321@mail.ru>
2024-03-18 15:50:32 +0700

    import StringUtils

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,16 @@
 package com.example;
 
+import org.apache.commons.lang3.StringUtils;
+
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String text = "Hello World";
+        System.out.println(text);
+        System.out.println(StringUtils.reverse(text));
+
     }
 }
